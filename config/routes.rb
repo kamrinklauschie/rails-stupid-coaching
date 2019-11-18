35dce7002910f7e5826cde8713fe9f320038bc9c
Kamrin Klauschie <kamrin.klauschie@gmail.com>
2019-11-18 16:00:11 -0300

    Implemented coaching MVC model

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'ask', to: 'questions#ask', as: :ask
+  # post 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer', as: :answer
+  # get 'answer/:id', to: 'questions#show'
+
+  root to: 'questions#home'
 end
